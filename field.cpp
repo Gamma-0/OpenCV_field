fd35dc5518b92570fe1efb21b39c8ee7776e329f
Nouur <nour._.grati@hotmail.fr>
2018-01-08 00:36:33 +0100

    Primary Field dettio n works on photos in log1 : from 001 to 102

--- field.cpp
+++ field.cpp
@@ -16,37 +16,67 @@ void process(const char* ims)
 
 	imshow(ims, img_in);
 	waitKey();
-
+	
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
 	// Convertir l'image dans l'espace HSV
 	Mat img_hsv;
 	cvtColor(img_in, img_hsv, CV_BGR2HSV);
-
-
+	
+	
 	// Modifie les canaux HSV par les paramètres
 	for (int i=0; i < img_hsv.rows ; i++) {
-		for(int j=0; j < img_hsv.cols; j++) {
-			img_hsv.at<Vec3b>(i,j)[1] = 255;
+	  for(int j=0; j < img_hsv.cols; j++) {
+	    img_hsv.at<Vec3b>(i,j)[1] = 255;
 			img_hsv.at<Vec3b>(i,j)[2] = 255;
-		}
+	  }
 	}
-
+	
 	// Sauvegarde la nouvelle image imd
 	cvtColor(img_hsv, img_hsv, CV_HSV2BGR);
-
+	
 	imshow(ims, img_hsv);
 	waitKey();
-
+	
 	inRange(img_hsv, Scalar(0, 210, 0), Scalar(255, 255, 255), img_hsv); //Threshold the image
 	imshow(ims, img_hsv);
 
 	waitKey();
-
+	
 	erode(img_hsv, img_hsv, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
 	dilate( img_hsv, img_hsv, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
-	imshow(ims, img_hsv);
-
+	
+	imshow(ims,img_hsv );
 	waitKey();
 
+
+	Mat canny_output;
+	int thresh = 100;
+	int max_thresh = 255;
+	/// Detect edges using canny
+	Canny(  img_hsv, canny_output, thresh, thresh*2, 3 );
+	imshow(ims,canny_output);
+	waitKey();
+
+	
+	/// Find contours
+	findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );  
+	/// Draw contours
+	int max=0;
+	int max_index=0;
+	if(contours.size()>2)
+	for( int i = 0; i< contours.size(); i++ ){
+	  
+	  if(contours[i].size()>max){
+	    max =contours[i].size();
+	    max_index=i;
+	  }
+	}
+	drawContours( img_in, contours, max_index,255 , 2, 8, hierarchy, 0, Point() );
+	
+	imshow(ims,img_in);
+	waitKey();
+	
 }
 
 void usage (const char *s)
