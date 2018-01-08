c3f46f805a99f563866d03c27d9448871e69aa76
SEOANE Margaux <mseoane@grandchelem.pedago.ipb.fr>
2018-01-08 15:39:22 +0100

    Code updated

---
+++ README.md
@@ -0,0 +1,13 @@
+# Lot 1 : Terrain
+
+
+## Membres de l'équipe
+- Nour Grati
+- Marion Mounaix
+- Margaux Seoane
+
+## Notice d'utilisation
+ Todo...
+1. qmake-qt4 field.pro
+2. make
+3. ./field img.png
--- field.cpp
+++ field.cpp
@@ -16,93 +16,81 @@ void process(const char* ims)
 
 	imshow(ims, img_in);
 	waitKey();
-	
+
 	vector<vector<Point> > contours;
 	vector<Vec4i> hierarchy;
 	// Convertir l'image dans l'espace HSV
 	Mat img_hsv;
 	cvtColor(img_in, img_hsv, CV_BGR2HSV);
-	
-	
+
+
 	// Modifie les canaux HSV par les paramètres
 	for (int i=0; i < img_hsv.rows ; i++) {
-	  for(int j=0; j < img_hsv.cols; j++) {
-	    img_hsv.at<Vec3b>(i,j)[1] = 255;
+		for (int j=0; j < img_hsv.cols; j++) {
+			img_hsv.at<Vec3b>(i,j)[1] = 255;
 			img_hsv.at<Vec3b>(i,j)[2] = 255;
-	  }
+		}
 	}
 
-	
 	// Sauvegarde la nouvelle image imd
 	cvtColor(img_hsv, img_hsv, CV_HSV2BGR);
-	
+
 	imshow(ims, img_hsv);
 	waitKey();
-	
+
 	inRange(img_hsv, Scalar(0, 210, 0), Scalar(255, 255, 255), img_hsv); //Threshold the image
 	imshow(ims, img_hsv);
 
 	waitKey();
 
-
-
-	Scalar m= mean(img_hsv);
+	Scalar m = mean(img_hsv);
 	cerr<<"mean= "<<m<<endl;
 
+	if (m[0] <= 245){
+		erode(img_hsv, img_hsv, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
+		dilate( img_hsv, img_hsv, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
 
-	if(m[0]<=250){
+		imshow(ims,img_hsv );
+		waitKey();
 
-	  erode(img_hsv, img_hsv, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
-	dilate( img_hsv, img_hsv, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
-	
-	imshow(ims,img_hsv );
-	waitKey();
+		Mat canny_output;
+		int thresh = 100;
+		/// Detect edges using canny
+		Canny(  img_hsv, canny_output, thresh, thresh*2, 3 );
+		imshow(ims,canny_output);
+		waitKey();
 
+		/// Find contours
+		findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+		/// Draw contours
+		unsigned int max=0;
+		int max_index=0;
 
-	Mat canny_output;
-	int thresh = 100;
+		/// match close edges
+		for(unsigned int i = 0; i< contours.size(); i++ ){
+			drawContours(canny_output, contours, i, 255, 2, 8, hierarchy, 0, Point() );
+		}
 
-	/// Detect edges using canny
-	Canny(  img_hsv, canny_output, thresh, thresh*2, 3 );
-	imshow(ims,canny_output);
-	waitKey();
+		imshow(ims,canny_output);
+		waitKey();
 
+		findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+		for(unsigned int i = 0; i < contours.size(); i++ ){
+			if(contours[i].size() > max){
+				max = contours[i].size();
+				max_index = i;
+			}
+		}
+		drawContours(img_in, contours, max_index, 255, 2, 8, hierarchy, 0, Point());
 
-
-	/// Find contours
-	findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );  
-	/// Draw contours
-	unsigned int max=0;
-	int max_index=0;
-	
-
-	
-	/// match close edges
-	for(unsigned int i = 0; i< contours.size(); i++ ){
-	  drawContours(canny_output, contours, i    ,255 , 2, 8, hierarchy, 0, Point() );
+		imshow(ims,img_in);
+		waitKey();
+	} else {
+		cerr<< "tout est terrain"<<endl;
+		copyMakeBorder(img_in, img_in, 2, 2, 2, 2, BORDER_CONSTANT, 255);
+		imshow(ims, img_in);
+		waitKey();
 	}
-	
-	imshow(ims,canny_output);
-	waitKey();
-
-	findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );  
-	for(unsigned int i = 0; i< contours.size(); i++ ){
-	  if(contours[i].size()>max){
-	    max =contours[i].size();
-	    max_index=i;
-	  }
-	}
-       	drawContours( img_in, contours, max_index,255 , 2, 8, hierarchy, 0, Point() );
-	
-	imshow(ims,img_in);
-	waitKey();
-	}
-	else 
-	  { cerr<< "tout est terrain"<<endl;
-	    copyMakeBorder(img_in,img_in, 2, 2, 2, 2,BORDER_CONSTANT, 255);
-	imshow(ims,img_in);
-	waitKey();
-	  }
 }
 
 void usage (const char *s)
